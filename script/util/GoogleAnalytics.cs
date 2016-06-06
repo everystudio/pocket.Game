f4437f3695db68829d36908c70c22a0678dfd098
watanabe.everystudio <watanabe.everystudio@gmail.com>
2016-06-06 20:15:28 +0900

    Analyticsの集計をちょっと変更

--- script/util/GoogleAnalytics.cs
+++ script/util/GoogleAnalytics.cs
@@ -6,12 +6,16 @@ public class GoogleAnalytics : GoogleAnalyticsBase<GoogleAnalytics> {
 	public override void Initialize ()
 	{
 		base.Initialize ();
+		propertyID_Common = "UA-77286676-7";
 		propertyID_Android = "UA-77286676-1";
 		propertyID_iOS = "UA-77286676-2";
 
 		bundleID = "jp.everystudio.pocket.zoo";
 		appName = "ポケット動物園";
 		appVersion = "1";
+
+		// ハートビート
+		heartbeat ();
 	}
 
 }
